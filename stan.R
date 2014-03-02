b302e8925c511374c2239d1a4831d70380f963ed
Alexander Jerneck <ajerneck@sas.upenn.edu>
2014-03-02 17:17:00 -0500

    Inspecting the posterior of the bernoulli model with different priors.

--- stan.R
+++ stan.R
@@ -10,9 +10,9 @@ library(plyr)
 library(reshape2)
 set_cppo('fast')
 
-########################################
-## Bernoulli example from the manual. ##
-########################################
+###################################################################
+## Bernoulli example from the manual, but with different priors. ##
+###################################################################
 ## ~/stan-reference-2.2.0.pdf
 
 priors <- list(
@@ -51,14 +51,27 @@ ms <- lapply(codes, function(x) stan(model_code=x, data=bern_data, iter=2000, ch
 
 ## examine fitted models.
 
-msd <- ldply(ms, function(x) {
-    sims <- melt(data.frame(extract(x, permuted=TRUE, pars='theta'), varname='sim'))
+model_data <- function(x) {
+    sims <- melt(data.frame(extract(x, permuted=TRUE), varname='sim'))
     sums <- melt(summary(x)$summary)
     names(sums) <- c('variable','varname','value')
     rbind(sims, sums)
-})
+}
+
+msd <- ldply(ms, model_data)
+
+model_summary <- function(x) {
+    msdd <- dcast(.id ~ varname, data=x[x$varname != 'sim',])
+    names(msdd) <- str_replace(names(msdd), '%', 'pc')
+    names(msdd) <- str_replace(names(msdd), '(^[0-9])', 'b\\1')
+    msdd
+}
 
 msd <- msd[msd$variable == 'theta',]
+msdd <- model_summary(msd)
+
+ggplot(msd) + geom_histogram(aes(value, fill=.id), data=msd[msd$varname == 'sim',]) + facet_wrap(~.id) + geom_vline(aes(xintercept=mean), linetype='dotted', data=msdd) + geom_segment(aes(x=b2.5pc, xend=b97.5pc, y=0, yend=0), data=msdd) + theme_bw()
+
 
 msdd <- dcast(.id ~ varname, data=msd[msd$varname != 'sim',])
 names(msdd) <- str_replace(names(msdd), '%', 'pc')
